11cecb3f28265ad6ac0014f7421c5a13ae42b8c7
Psycological <aryakaushal2004@gmail.com>
2023-05-16 21:13:00 +0530

    modified

--- stack reverse a string.cpp
+++ stack reverse a string.cpp
@@ -4,13 +4,19 @@
 
 void reverse(const std::string& a) {
     std::stack<std::string> b;
+//initialiing a empty string to enter the reversed 
+//string  
     std::string word = "";
-
+//loop for for reversing the string
     for (int i = 0; i < a.length(); i++) {
+//till  a space occeurs in program it continues to 
+//add the word into the empty string
         while (a[i] != ' ' && i < a.length()) {
             word += a[i];
             i++;
         }
+//word is push and because of lifo property 
+//the last word is printed first
         b.push(word);
         word = "";
     }
