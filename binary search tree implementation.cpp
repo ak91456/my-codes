f4dd8eec5780d5c66c8f1e6fc0621943291a53e1
Psycological <aryakaushal2004@gmail.com>
2023-05-16 19:06:06 +0530

    modified

--- binary search tree implementation.cpp
+++ binary search tree implementation.cpp
@@ -1,47 +1,68 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
-    public:
+class node
+{
+public:
     int data;
-    node*left,*right;
-    node(int val){
-        data=val;
-        right=NULL;
-        left=NULL;
+    node *left, *right;
+    node(int val)
+    {
+        data = val;
+        right = NULL;
+        left = NULL;
     }
 };
-node *bt(node *root,int val){
-    if(root==NULL){
+node *bt(node *root, int val)
+{
+    if (root == NULL)
+    {
         return new node(val);
     }
-    if(val<root->data){
-        root->left=bt(root->left,val);
+    if (val < root->data)
+    {
+        root->left = bt(root->left, val);
     }
-    else{
-        root->right=bt(root->right,val);
+    else
+    {
+        root->right = bt(root->right, val);
     }
     return root;
 }
-void inorder(node* root){
-    if(root==NULL){
+void inorder(node *root)
+{
+    if (root == NULL)
+    {
         return;
     }
     inorder(root->left);
-    cout<<root->data<<"->";
+    cout << root->data << "->";
     inorder(root->right);
 }
-int main(){
-    int a;
-    cout<<"enter the value of root : ";
-    cin>>a;
-    node *root=new node(a);
-    int count;
-    cout<<"Enter the number of elements : ";
-    cin>>count;
-    cout<<"enter values : \n";
-    for(int i=0;i<count;i++){
-        cin>>a;
-        bt(root,a);
-    }
+node *insertion()
+{
+    node *root = nullptr;
+    int value;
+    cout << "enter a value : ";
+    cin >> value;
+    root = bt(root, value);
+    char choice;
+    do
+    {
+        cout << "do u want to enter another node (y/n) : ";
+        cin >> choice;
+        if (choice == 'y')
+        {
+            cout << "enter a value : ";
+            cin >> value;
+            root = bt(root, value);
+        }
+    } while (choice == 'y');
+    return root;
+}
+int main()
+{
+    node *root = insertion();
+    cout << "\nThe inorder binary search tree traversal is : \n";
     inorder(root);
+    return 0;
 }
